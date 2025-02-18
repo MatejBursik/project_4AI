3300bf2d5c3c6cfc3337cc955594657f0f15db9b
MatejBursik <r0929146@student.thomasmore.be>
2025-02-18 08:39:53 +0100

    killing bugs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ location_id = os.getenv("loc_id")
 # Load the YOLOv11 model
 model = YOLO("YOLO_model/first_model.onnx")
 
-path = 0 # path for camera = 0, path for video = "test_data/multi_color_test.mp4"
+path = "/dev/video1" # path for camera = 0, path for video = "test_data/multi_color_test.mp4"
 cap = cv2.VideoCapture(path)
 
 # Save as video
